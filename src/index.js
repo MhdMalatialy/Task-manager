daeb0dd934f1ee5391aaa0735c84913e0377ba51
MhdMalatialy <67335521+MhdMalatialy@users.noreply.github.com>
2021-01-01 11:20:55 +0200

    make it better

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ const { ObjectID } = require('mongodb')
 const User = require('./models/user')
 
 const app=express()
-
-const port=process.env.PORT 
 app.use(express.json())
 app.use(userRauter)
 app.use(taskRauter)
-app.listen(port,()=>console.log('server is up on port:'+port))
+app.listen(process.env.PORT, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
