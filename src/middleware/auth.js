e023c51f8721ad5244547d69027571ffbddbbd0e
MhdMalatialy <67335521+MhdMalatialy@users.noreply.github.com>
2021-01-01 14:00:43 +0200

    fix typo

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,10 @@
-const jwt = require('jsonWebToken') 
+const jwt = require('jsonwebtoken') 
 const User = require("../models/user")
 const auth = async (req,res,next) => {
 try {
     const token = req.header('Authorization').replace('Bearer ','')
     const decode = jwt.verify(token,process.env.JWTSECRET)
-    const user = await User.findOne({_id:decode._id,'tokens.token':token})
+    const user = await User.findOne({_id:decode._id,'tokens.token':token} )
     if (!user) { 
         throw new Error()
     } 
